dbdd2c41a97da337c2aef2b9a1642a1722272388
Markus <kikarn@gmail.com>
2024-01-04 14:35:03 +0100

    Removed rules from sec group

--- terraform/security.tf
+++ terraform/security.tf
@@ -70,20 +70,6 @@ resource "aws_security_group" "SG-testburk" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-    ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   #Allow all outbound traffic
   egress {
     from_port   = 0
